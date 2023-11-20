d53162fcd807f6d748bc0786be2f0e98dd238ddd
Davel Reymundo <davel.reymundo@gmail.com>
2023-11-20 15:44:08 -0500

    feat: Configure environment, typeorm and modules in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TeacherModule } from './teacher/teacher.module';
+import { CourseModule } from './course/course.module';
+import { StudentModule } from './student/student.module';
+import { EnrollmentModule } from './enrollment/enrollment.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env'] }), //{ isGlobal: true }
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+    }),
+    TeacherModule,
+    CourseModule,
+    StudentModule,
+    EnrollmentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
