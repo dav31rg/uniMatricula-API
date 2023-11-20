48e3d9bcd82531fe8eb06b933a2d61f8a05b41ab
Davel Reymundo <davel.reymundo@gmail.com>
2023-11-20 16:25:58 -0500

    feat: Add decorators, relationships for teacher entity

---
+++ src/teacher/entities/teacher.entity.ts
@@ -0,0 +1,33 @@
+import { Course } from 'src/course/entities/course.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Teacher {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+
+  @OneToMany(() => Course, (course) => course.teacher)
+  courses: Course[];
+}
