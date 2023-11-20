233cc91c1106cfa839f1159b8f33ecbe4c3e9973
Davel Reymundo <davel.reymundo@gmail.com>
2023-11-20 16:14:15 -0500

    feat: Add decorators, relationships for enrollment entity

---
+++ src/enrollment/entities/enrollment.entity.ts
@@ -0,0 +1,37 @@
+import { Course } from 'src/course/entities/course.entity';
+import { Student } from 'src/student/entities/student.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Enrollment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  semester: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+
+  @ManyToOne(() => Student, (student) => student.enrollments)
+  student: Student;
+
+  @ManyToOne(() => Course, (course) => course.enrollments)
+  course: Course;
+}
