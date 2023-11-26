3ade89520100017c3ca73ac4cbded3a05208a22d
Davel Reymundo <davel.reymundo@gmail.com>
2023-11-26 01:15:56 -0500

    feat: Add erd

--- src/teacher/dto/teacher.dto.ts
+++ src/teacher/dto/teacher.dto.ts
@@ -3,7 +3,6 @@ import { ApiProperty, PartialType } from '@nestjs/swagger';
 export class CreateTeacherDto {
   @ApiProperty()
   readonly name: string;
-  // readonly courseId: number[];
 }
 
 export class UpdateTeacherDto extends PartialType(CreateTeacherDto) {}
