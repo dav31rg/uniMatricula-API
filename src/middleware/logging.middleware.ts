4a563da119547d79c54aabe96d931e3f202bee0a
Davel Reymundo <davel.reymundo@gmail.com>
2023-11-27 13:35:52 -0500

    refactor: Delete console logs in middleware

--- src/middleware/logging.middleware.ts
+++ src/middleware/logging.middleware.ts
@@ -1,13 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
+import * as fs from 'fs';
 
 export function logger(req: Request, res: Response, next: NextFunction) {
-  const startTime = Date.now();
-  console.log(`tiempo inicio: ${startTime}`);
+  if (process.env.LOGGER_ENABLED === 'true') {
+    const startTime = Date.now();
 
-  res.on('finish', () => {
-    const endTime = Date.now();
-    console.log(`tiempo final: ${endTime}`);
-    console.log(`Duracion: ${endTime - startTime}`);
-  });
+    res.on('finish', () => {
+      const endTime = Date.now();
+      const logTime = `Request:${req.method}, endpoint:${req.path}, Duration:${
+        endTime - startTime
+      }\n`;
+      fs.appendFile('log.csv', logTime, (err) => {
+        if (err) throw err;
+      });
+    });
+  }
   next();
 }
